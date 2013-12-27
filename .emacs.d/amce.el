95087f7e7b16d6adf7a5637b7a90c3b261142fd4
Adrian Marina <adrianmarina.adrian@gmail.com>
2013-12-27 01:18:19 +0000

    added basic emacs files

---
+++ .emacs.d/amce.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
+(load "amce/theme")
+(load "amce/global")
+(load "amce/defuns")
+(load "amce/disabled")
+(load "amce/fonts")
---
+++ .emacs.d/amce/defuns.el
@@ -0,0 +1,47 @@
+;;; Personal functions
+
+;; For loading libraries from the vendor directory
+;; Modified from defunkt's original version to support autoloading.
+;; http://github.com/defunkt/emacs/blob/master/defunkt/defuns.el
+(defun vendor (library &rest autoload-functions)
+  (let* ((file (symbol-name library))
+	 (normal (concat "~/.emacs.d/vendor/" file))
+	 (suffix (concat normal ".el"))
+	 (personal (concat "~/.emacs.d/rmm5t/" file))
+	 (found nil))
+    (cond
+     ((file-directory-p normal) (add-to-list 'load-path normal) (set 'found t))
+     ((file-directory-p suffix) (add-to-list 'load-path suffix) (set 'found t))
+     ((file-exists-p suffix)  (set 'found t)))
+    (when found
+      (if autoload-functions
+	  (dolist (autoload-function autoload-functions)
+	    (autoload autoload-function (symbol-name library) nil t))
+	(require library)))
+    (when (file-exists-p (concat personal ".el"))
+      (load personal))))
+
+;; Quickly jump back and forth between matching parens/brackets
+(defun match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+	((looking-at "\\s\)") (forward-char 1) (backward-list 1))))
+
+;; Make the whole buffer pretty and consistent
+(defun iwb()
+  "Indent Whole Buffer"
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+;; Borrowed from http://whattheemacsd.com/key-bindings.el-01.html
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+	(linum-mode 1)
+	(goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
