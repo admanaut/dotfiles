3deb04935d703efbea7853e4209bad806961cca2
Adrian Marina <adrianmarina.adrian@gmail.com>
2015-12-01 08:12:21 +0000

    remove custom guide-key idle-delay

--- emacs.d/amce/guide-key.el
+++ emacs.d/amce/guide-key.el
@@ -5,10 +5,8 @@
 (require 'guide-key)
 ;; prefix key
 (setq guide-key/guide-key-sequence '("C-x"))
-(setq guide-key/idle-delay 0.5)
 (setq guide-key/highlight-command-regexp
       '("rectangle"
         ("register" . font-lock-type-face)
         ("bookmark" . "hot pink")))
-
 (guide-key-mode 1)
