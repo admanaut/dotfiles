ca9a9160c1d230bc995c279ea5b023f0f74e87c7
Adrian Marina <adrianmarina.adrian@gmail.com>
2016-05-16 00:31:24 +0100

    add haskell mode; configure helm;

--- emacs.d/amce/packages.el
+++ emacs.d/amce/packages.el
@@ -25,6 +25,9 @@
     cider
     company
 
+    ;; haskell
+    haskell-mode
+
     ;; general
     golden-ratio
     magit
@@ -67,13 +70,28 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; helm
+(require 'helm)
 (require 'helm-config)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(helm-mode 1)
-(helm-autoresize-mode 1)
 
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebihnd tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+;(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+;; disable helm completion
+(setq helm-mode-handle-completion-in-region nil)
+(helm-mode 1)
+(global-set-key (kbd "M-x") 'helm-M-x)
 ;; use ido for find file
 (add-to-list 'helm-completing-read-handlers-alist '(find-file . ido))
+
+(setq helm-quick-update                     t ; do not display invisible candidates
+      helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-adaptive-history-file            "~/.emacs.d/data/helm-history")
+
 
 ;; Golden ratio
 (require 'golden-ratio)
@@ -82,3 +100,8 @@
 ;; Magit
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
+
+;; Haskell mode
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
