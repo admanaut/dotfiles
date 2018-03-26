5fdfc194084cb92d4b4290c1ec37b85698eae3f5
Adrian Marina <adrianmarina.adrian@gmail.com>
2018-03-26 22:07:04 +0100

    Haskell Intero mode
    plus some clean up

--- emacs.d/amce/global.el
+++ emacs.d/amce/global.el
@@ -40,13 +40,9 @@
 ;; (setq visible-bell t)
 
 ;; Make sure all backup files only live in one place
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups"))
-      backup-by-copying t    ; Don't delink hardlinks
-      version-control t      ; Use version numbers on backups
-      delete-old-versions t  ; Automatically delete excess backups
-      kept-new-versions 6   ; how many of the newest versions to keep
-      kept-old-versions 2    ; and how many of the old
-      )
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq auto-save-file-name-transforms
+          `((".*" ,"~/.emacs.d/auto-save")))
 
 ;; Gotta see matching parens
 (show-paren-mode t)
--- emacs.d/amce/packages.el
+++ emacs.d/amce/packages.el
@@ -29,14 +29,11 @@
     clojure-mode
     clojure-mode-extra-font-locking
     cider
-    company
     clj-refactor
 
     ;; haskell
-    ;;haskell-mode
-    ;;flycheck-haskell
-    ;;company-ghc
-    intero ;; enhanced haskell mode
+    haskell-mode
+    intero
 
     ;; purescript
     purescript-mode
@@ -45,14 +42,17 @@
 
     ;; general
     golden-ratio
+    company
     magit
     helm
     ido-vertical-mode
     restclient
     ace-window
     json-mode
-    flycheck
-    ;;repl-toggle
+    projectile
+    neotree
+    eyebrowse
+    avy
 
     ;;golang
     go-mode
@@ -108,17 +108,17 @@
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'clojure-mode-hook #'company-mode)
 
-;; ~~~~~~ Company ~~~~~~
-
-(setq company-idle-delay 0.3)
+;; Company
 ;;(setq company-idle-delay nil) ; never start completions automatically
+(setq company-idle-delay 0.3)
 ;;(global-set-key (kbd "M-TAB") #'company-complete) ; use meta+tab, aka C-M-i, as manual trigger
 
 ;; Magit
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; helm
+;; ====  Helm ====
+
 (require 'helm)
 (require 'helm-config)
 
@@ -128,6 +128,7 @@
 ;; disable helm completion
 (setq helm-mode-handle-completion-in-region nil)
 (helm-mode 1)
+
 (global-set-key (kbd "M-x") 'helm-M-x)
 ;; use ido for find file
 (add-to-list 'helm-completing-read-handlers-alist '(find-file . ido))
@@ -141,19 +142,21 @@
       helm-ff-file-name-history-use-recentf t
       helm-adaptive-history-file            "~/.emacs.d/data/helm-history")
 
+;; ====  Fiplr ====
+(require 'fiplr)
+(global-set-key (kbd "C-c f f") 'fiplr-find-file)
+
+;; ====  Ido ====
+
 (require 'ido-vertical-mode)
 (ido-vertical-mode t)
 (setq ido-vertical-define-keys 'C-n-and-C-p-only)
 
-;; Golden ratio
+;; ==== Golden ratio ====
 (require 'golden-ratio)
 (golden-ratio-mode 1)
 
-;; Magit
-(require 'magit)
-(global-set-key (kbd "C-x g") 'magit-status)
-
-;; clj-refactor
+;; ====  Clj-Refactor ====
 (require 'clj-refactor)
 (defun clj-refactor-clojure-mode ()
     (clj-refactor-mode 1)
@@ -170,8 +173,8 @@
 (add-to-list 'golden-ratio-extra-commands 'magit-status)
 
 ;; find-file-at-point, useful with grep and find results
-;;(require 'ffap)
-;;(global-set-key (kbd "C-c p") 'find-file-at-point)
+(require 'ffap)
+(global-set-key (kbd "C-c p") 'find-file-at-point)
 
 ;; GEISER - for scheme and racket
 (require 'geiser)
@@ -193,3 +196,90 @@
 
 ;; erlang initializations
 (require 'erlang-start)
+
+;; ============= Haskell mode ================
+
+(require 'intero)
+(add-hook 'haskell-mode-hook 'intero-mode)
+;; TODO (setq intero-blacklist '("~/code/haskell-project/"))
+;; TODO (add-hook 'haskell-mode-hook 'intero-mode-whitelist)
+
+
+;; =========== Purescript ========
+
+(require 'psc-ide)
+(add-hook 'purescript-mode-hook
+  (lambda ()
+    (psc-ide-mode)
+    (company-mode)
+    (flycheck-mode)
+    (turn-on-purescript-indentation)))
+
+;; use the exacutables relative to node_modules/.bin
+(setq psc-ide-use-npm-bin t)
+(setq psc-ide-add-import-on-completion 0)
+
+;; ==== CSS ====
+(setq css-indent-offset 2)
+(add-hook 'css-mode #'company-mode)
+
+
+;; ====  Window ====
+
+(global-set-key (kbd "s-<left>")  'windmove-left)
+(global-set-key (kbd "s-<right>") 'windmove-right)
+(global-set-key (kbd "s-<up>")    'windmove-up)
+(global-set-key (kbd "s-<down>")  'windmove-down)
+
+;; ====  AG ====
+(require 'ag)
+(setq ag-reuse-buffers 't)
+
+(global-set-key (kbd "C-c a f") 'ag-files)         ;; search [string] files [matching] in [dir]
+(global-set-key (kbd "C-c a p") 'ag-project)       ;; search [string] in project (.git)
+(global-set-key (kbd "C-c a m") 'ag-project-files) ;; search [string] in project (.git) files [matching]
+
+;; ====  Multiple-Cursors ====
+
+(require 'multiple-cursors)
+
+;; active region that spans multiple lines
+(global-set-key (kbd "C-+") 'mc/edit-lines)
+
+;; add multiple cursors based on keywords in the buffer
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-?") 'mc/mark-all-like-this)
+
+;; ====  Projectile ====
+
+;; ====  NeoTree ====
+;; NeoTree can be opened (toggled) at projectile project root as follows
+(require 'neotree)
+(defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name)))
+        (message "Could not find git project root."))))
+
+(global-set-key [f8] 'neotree-project-dir)
+
+;; ====  eyebrowse ====
+(require 'eyebrowse)
+(eyebrowse-mode t)
+
+;; ==== Desktop  ====
+(desktop-save-mode 1)
+
+;; avy
+(require 'avy)
+(global-set-key (kbd "C-;") 'avy-goto-char-timer)
+(global-set-key (kbd "C-:") 'avy-goto-char)
+(global-set-key (kbd "C-'") 'avy-goto-char-2)
